a22ad0fe46aa809f50fc427d3f8a6d821fac3032
migueltomarnogueira <tomarnogueiracoding@gmail.com>
2022-08-26 17:44:45 +0100

    All backend routes for Quotes created and working

--- routes/quote.routes.js
+++ routes/quote.routes.js
@@ -5,7 +5,7 @@ const Quote = require('../models/Quote.model');
 const User = require('../models/User.model');
 const quoteAPI = 'https://api.quotable.io';
 
-// Get Random Quote
+// Get Random Quote from external API
 router.get('/quotes/random', (req, res, next) => {
   axios
     .get(`${quoteAPI}/random`)
@@ -13,21 +13,19 @@ router.get('/quotes/random', (req, res, next) => {
     .catch((err) => res.json(err));
 });
 
-// Search a Quote
+// Search a Quote from external API
 router.get('/quotes/search', (req, res, next) => {
   const { query } = req.query;
-
   axios
     .get(`${quoteAPI}/search/quotes?query=${query}`)
     .then((response) => res.status(200).json(response.data))
     .catch((err) => {
       res.json(err);
-      console.log(err);
     });
 });
 
 // Create a Quote in Database and send to User favorites
-router.post('/quotes', (req, res, next) => {
+router.post('/quotes/addFavorite', (req, res, next) => {
   const { content, author, minLength, maxLength, userId } = req.body;
 
   Quote.create({ content, author, minLength, maxLength })
@@ -37,6 +35,52 @@ router.post('/quotes', (req, res, next) => {
       });
     })
     .then((response) => res.status(201).json(response))
+    .catch((err) => res.json(err));
+});
+
+//Get all Favorite Quotes from a User
+router.get('/quotes/all-favorites/:userId', (req, res, next) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .populate('favorites')
+    .then((favoriteQuotes) => res.status(200).json(favoriteQuotes))
+    .catch((err) => res.json(err));
+});
+
+//Get One Quote by Id from the Database
+router.get('/quotes/:quoteId', (req, res, next) => {
+  const { quoteId } = req.params;
+
+  Quote.findById(quoteId)
+    .then((favoriteQuote) => res.status(200).json(favoriteQuote))
+    .catch((err) => res.json(err));
+});
+
+// Edit One Quote by ID from the Database
+router.put('/quotes/:quoteId', (req, res, next) => {
+  const { quoteId } = req.params;
+  const { content, author, minLength, maxLength } = req.body;
+
+  Quote.findByIdAndUpdate(
+    quoteId,
+    { content, author, minLength, maxLength },
+    { new: true }
+  )
+    .then((favoriteQuote) => res.status(201).json(favoriteQuote))
+    .catch((err) => res.json(err));
+});
+
+// Delete One Quote by ID from the Database
+router.delete('/quotes/:quoteId', (req, res, next) => {
+  const { quoteId } = req.params;
+
+  Quote.findByIdAndDelete(quoteId)
+    .then(() =>
+      res.status(200).json({
+        message: `The quote with the id ${quoteId} was sucefully deleted`,
+      })
+    )
     .catch((err) => res.json(err));
 });
 
